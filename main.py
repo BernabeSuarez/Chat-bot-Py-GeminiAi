f38918430cec1d0002cbee4fa029ed75255cdc18
Bernabe Suarez <bernatattoo@gmail.com>
2024-09-22 01:36:18 -0300

    change model

--- main.py
+++ main.py
@@ -31,9 +31,22 @@ safety_settings = [
     },
 ]
 
-
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
 def gemini_consult(query):
-    model = genai.GenerativeModel("gemini-1.5-flash")
+   model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
     response = model.generate_content(query, safety_settings=safety_settings)
     return response
 
